50d3472fd9f2fbedfe6ff7af269e9890a6fb8be1
Ruben Emilio Diaz Martinez <32526357+RUBENEMILIO100@users.noreply.github.com>
2020-06-10 16:46:25 -0700

    Add files via upload

---
+++ Evaluation(Exam)/EvaluationUnit3.scala
@@ -0,0 +1,57 @@
+// Evaluation Unit 3
+// Diaz Martinez Ruben Emilio # 15210791 Flores Acosta Alfredo # 15210331
+ 
+
+
+// # 1- Import a simple Spark session.
+
+import org.apache.spark.sql.SparkSession
+
+//                        ^
+// Spark Session -------- ^
+
+// # 2 Use the lines of code to minimize errors
+
+import org.apache.log4j._
+Logger.getLogger("org").setLevel(Level.ERROR)
+
+// # 3 Create an instance of the Spark session
+
+val spark = SparkSession.builder().getOrCreate()
+
+// # 4 Import the Kmeans library for the grouping algorithm. 
+
+import org.apache.spark.ml.clustering.KMeans
+
+// # 5 Loads the Wholesale Customers Data dataset
+// From https://github.com/jcromerohdz/BigData/blob/master/Spark_clustering/Wholesale%20customers%20data.csv 
+
+val dataset = spark.read.option("header","true").option("inferSchema","true").format("csv").load("Wholesale customers data.csv")
+
+// # 6 Select the following columns: Fresh, Milk, Grocery, Frozen, Detergents_Paper,
+// Delicassen and call this set feature_data
+val feature_data = dataset.select($"Fresh", $"Milk", $"Grocery", $"Frozen", $"Detergents_Paper", $"Delicassen")
+
+// # 7 Import Vector Assembler and Vector
+import org.apache.spark.ml.feature.VectorAssembler
+import org.apache.spark.ml.linalg.Vectors
+
+// # 8 Create a new Vector Assembler object for feature columns as a
+// input set, remembering that there are no labels
+val assembler = new VectorAssembler().setInputCols(Array("Fresh", "Milk", "Grocery", "Frozen", "Detergents_Paper", "Delicassen")).setOutputCol("features")
+
+// # 9 Use the assembler object to transform feature_data
+val traning = assembler.transform(feature_data)
+
+// # 10 Create a Kmeans model with K = 3
+val kmeans = new KMeans().setK(3).setSeed(1L)
+val model = kmeans.fit(traning)
+
+// # 11 Evaluate the groups using Within Set Sum of Squared Errors WSSSE and print the centroids.
+// Squared Errors
+val WSSSE = model.computeCost(traning)
+println(s"Within Set Sum of Squared Errors = $WSSSE")
+
+// Centers
+println("Cluster Centers: ")
+model.clusterCenters.foreach(println)
